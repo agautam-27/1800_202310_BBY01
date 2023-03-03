5d4b3c9eda73922b7c46f5641d22e04fea48e14a
Anshul Gautam <agautam9@my.bcit.ca>
2023-03-02 18:19:22 -0800

    added the login, home, about, contact, index FAQ page with some css and firestore setup

---
+++ scripts/firebaseAPI_BBY01.js
@@ -0,0 +1,18 @@
+//----------------------------------------
+//  Your web app's Firebase configuration
+//----------------------------------------
+var firebaseConfig = {
+    apiKey: "AIzaSyDX9bKwQcSutAZCYahhEBp66qwp74IiNHQ",
+  authDomain: "team-project-bby01.firebaseapp.com",
+  projectId: "team-project-bby01",
+  storageBucket: "team-project-bby01.appspot.com",
+  messagingSenderId: "705307691312",
+  appId: "1:705307691312:web:bf6bf1ffd9cd531fb860f5"
+};
+
+//--------------------------------------------
+// initialize the Firebase app
+// initialize Firestore database if using it
+//--------------------------------------------
+const app = firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
