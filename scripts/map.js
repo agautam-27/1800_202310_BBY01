82142ae2292b95c32714dc0764870db1b38ec533
Anshul Gautam <agautam9@my.bcit.ca>
2023-03-26 23:30:58 -0700

    ping feature updated on the map to allow user choose a hazard option and be able to write comments

--- scripts/map.js
+++ scripts/map.js
@@ -70,7 +70,6 @@ map.addControl(new minimizeControl());
 
 
 
-//Marker
 var geojsonMarkerOptions = {
     radius: 8,
     fillColor: "#ff7800",
@@ -87,103 +86,121 @@ var longPressTimeout;
 
 // Add a marker to the map when the user clicks and holds for 3 seconds
 function onMapMouseDown(e) {
-    mouseDown = true;
-    longPressTimeout = setTimeout(function() {
-        if (mouseDown) {
-            var confirmCreateMarker = confirm("Do you want to mark a hazard here?");
-            if (confirmCreateMarker) {
-                var condition = prompt("What is the hazard? (snow, ice, flood)");
-                var now = new Date();
-                var dateTime = now.toLocaleString();
-                var popupContent = "<b>Condition: </b>" + condition + "<br><b>Created at: </b>" + dateTime;
-                var marker = L.marker(e.latlng).addTo(map);
-                marker.bindPopup(popupContent).openPopup();
+  mouseDown = true;
+  longPressTimeout = setTimeout(function() {
+    if (mouseDown) {
+      var confirmCreateMarker = confirm("Do you want to mark a hazard here?");
+      if (confirmCreateMarker) {
+        var conditions = ["snow", "ice", "flood"];
 
-                // Save the marker data to Firestore
-                var user = firebase.auth().currentUser;
-                if (user) {
-                    var markerData = {
-                        condition: condition,
-                        created_at: now,
-                        latitude: e.latlng.lat,
-                        longitude: e.latlng.lng,
-                    };
-                    // Add Points to users
-                    firestore
-                        .collection("users")
-                        .doc(user.uid)
-                        .collection("markers")
-                        .add(markerData)
-                        .then(function() {
-                            console.log("Marker added to Firestore.");
-                            // Increment user's points
-                            firestore
-                                .collection("users")
-                                .doc(user.uid)
-                                .update({ points: firebase.firestore.FieldValue.increment(1) })
-                                .then(function() {
-                                    console.log("User's points incremented.");
-                                })
-                                .catch(function(error) {
-                                    console.error("Error incrementing user's points:", error);
-                                });
-                        })
-                        .catch(function(error) {
-                            console.error("Error adding marker to Firestore:", error);
-                        });
-                } else {
-                    console.log("User not signed in.");
-                }
+        // Create a popup with the options
+        var popupContent = "Please select a condition:<br>";
+        for (var i = 0; i < conditions.length; i++) {
+          popupContent += "<input type='radio' name='condition' value='" + conditions[i] + "'>" + conditions[i] + "<br>";
+        }
+        popupContent += "<br><textarea id='comments-input' placeholder='Enter comments'></textarea><br>";
+        popupContent += "<button id='save-button'>Save</button>";
+
+        // Create the marker and add the popup
+        var marker = L.marker(e.latlng).addTo(map);
+        marker.bindPopup(popupContent).openPopup();
+
+        marker.on("popupopen", function() {
+          var saveButton = document.getElementById("save-button");
+          saveButton.addEventListener("click", function(event) {
+            var condition = document.querySelector("input[name='condition']:checked").value;
+            var comments = document.getElementById("comments-input").value;
+            var now = new Date();
+            var dateTime = now.toLocaleString();
+
+            var popupContent = "<b>Condition: </b>" + condition + "<br><b>Comments: </b>" + comments + "<br><b>Created at: </b>" + dateTime;
+            marker.setPopupContent(popupContent);
+
+            // Save the marker data to Firestore
+            var user = firebase.auth().currentUser;
+            if (user) {
+              var markerData = {
+                condition: condition,
+                comments: comments,
+                created_at: now,
+                latitude: e.latlng.lat,
+                longitude: e.latlng.lng,
+                user_id: user.uid // add this field to set the user ID
+              };
+              
+              // Add Points to users
+              firestore.collection("users").doc(user.uid).collection("markers").add(markerData).then(function(docRef) {
+                console.log("Marker added to Firestore.");
+
+                // Increment user's points
+                firestore.collection("users").doc(user.uid).update({ points: firebase.firestore.FieldValue.increment(1) }).then(function() {
+                  console.log("User's points incremented.");
+                }).catch(function(error) {
+                  console.error("Error incrementing user's points:", error);
+                });
+
+                // Add click event listener to marker to delete it
+                marker.on("click", function() {
+                  var confirmDeleteMarker = confirm("Do you want to delete this marker?");
+                  if (confirmDeleteMarker) {
+                    marker.remove(); // Remove the marker from the map
+                    firestore.collection("users").doc(user.uid).collection("markers").doc(docRef.id).delete().then(function() {
+                      console.log("Marker data deleted from Firestore.");
+                    }).catch(function(error) {
+                      console.error("Error deleting marker data from Firestore:", error);
+                    });
+                  }
+                });
+
+              }).catch(function(error) {
+                console.error("Error adding marker to Firestore:", error);
+              });
+            } else {
+              console.log("User not signed in.");
             }
+
+            marker.closePopup();
+         
+
+            });
+          });
+        }
         }
     }, 2000);
-}
-
-// Clear the timeout if the user releases the mouse button before the 2-second mark
-function onMapMouseUp() {
+  }
+  
+  function onMapMouseUp() {
     mouseDown = false;
     clearTimeout(longPressTimeout);
-}
-
-map.on('mousedown', onMapMouseDown);
-map.on('mouseup', onMapMouseUp);
-
-// const markersRef = firestore.collection("users").doc(user.uid).collection("markers");
-
-firebase.auth().onAuthStateChanged(function(user) {
-    if (user) {
-      const markersRef = firestore.collection("users").doc(user.uid).collection("markers");
+  }
   
-      markersRef.get().then((querySnapshot) => {
-        console.log("Retrieved markers:", querySnapshot.docs);
-        querySnapshot.forEach((doc) => {
-            const { latitude, longitude, condition, created_at } = doc.data();
-            const popupContent = `<b>Condition:</b> ${condition}<br><b>Created at:</b> ${created_at.toDate().toLocaleString()}`;
-            const marker = L.marker([latitude, longitude]).addTo(map);
-            marker.bindPopup(popupContent);
-        });
-      }).catch((error) => {
-        console.error("Error getting markers:", error);
-      });
-    }
-});
-
-//Gets every marker on Firestore and puts it on the map
-firebase.auth().onAuthStateChanged(function(user) {
+  map.on('mousedown', onMapMouseDown);
+  map.on('mouseup', onMapMouseUp);
+  
+  // Gets every marker on Firestore and puts it on the map
+  firebase.auth().onAuthStateChanged(function(user) {
     if (user) {
       const markersRef = firestore.collectionGroup("markers");
   
       markersRef.get().then((querySnapshot) => {
         console.log("Retrieved markers:", querySnapshot.docs);
         querySnapshot.forEach((doc) => {
-            const { latitude, longitude, condition, created_at } = doc.data();
-            const popupContent = `<b>Condition:</b> ${condition}<br><b>Created at:</b> ${created_at.toDate().toLocaleString()}`;
-            const marker = L.marker([latitude, longitude]).addTo(map);
-            marker.bindPopup(popupContent);
+          const { latitude, longitude, condition, comments, created_at, user_id } = doc.data();
+          const popupContent = `<b>Condition:</b> ${condition}<br> <b>Comment:</b> ${comments}<br><b>Created at:</b> ${created_at.toDate().toLocaleString()}`;
+          const isCurrentUserMarker = user_id === user.uid; // check if the current marker was created by the current user
+          const markerColor = isCurrentUserMarker ? "#00FF00" : "#ff7800"; // set a different marker color for the user's own markers
+          const marker = L.circleMarker([latitude, longitude], {
+            radius: 8,
+            fillColor: markerColor,
+            color: "#000",
+            weight: 1,
+            opacity: 1,
+            fillOpacity: 0.8
+          }).addTo(map);
+          marker.bindPopup(popupContent);
         });
       }).catch((error) => {
         console.error("Error getting markers:", error);
       });
     }
-});
-
+  });
