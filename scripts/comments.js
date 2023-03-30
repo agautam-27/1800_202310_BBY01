36f63343d1d3dbee99a248d583496e6e71f891db
Anshul Gautam <agautam9@my.bcit.ca>
2023-03-30 15:20:50 -0700

    updated the comments.js for better and precise resultsalong with that styling is also changed for comments.html

--- scripts/comments.js
+++ scripts/comments.js
@@ -17,52 +17,70 @@ function populateComments(userId) {
 
   // Fetch the documents from the markers subcollection
   markersRef.get().then((querySnapshot) => {
-    // Loop through each document in the markers subcollection and get its "comments" field
+    const commentsArr = [];
+    // Loop through each document in the markers subcollection and get its "comments", "created_at", "latitude", and "longitude" fields
     querySnapshot.forEach((markerDoc) => {
       const comments = markerDoc.data().comments;
-      // If comments is an array, loop through each comment and create a card for it
-      if (Array.isArray(comments)) {
-        comments.forEach((comment) => {
-          const card = document.createElement("div");
-          card.classList.add("card", "mb-3");
+      const condition = markerDoc.data().condition;
+      const created_at = markerDoc.data().created_at.toDate();
+      const latitude = markerDoc.data().latitude;
+      const longitude = markerDoc.data().longitude;
+      
+      commentsArr.push({
+        comments: comments,
+        condition: condition,
+        created_at: created_at,
+        latitude: latitude,
+        longitude: longitude
+      });
+    });
 
-          const cardBody = document.createElement("div");
-          cardBody.classList.add("card-body");
+    // Sort the comments array in descending order based on the created_at field
+    commentsArr.sort((a, b) => b.created_at - a.created_at);
 
-          const cardTitle = document.createElement("h5");
-          cardTitle.classList.add("card-title");
-          cardTitle.innerText = "Comment";
+    // Loop through each comment in the sorted comments array and create a card for it
+    commentsArr.forEach((commentObj) => {
+      const card = document.createElement("div");
+      card.classList.add("card", "mb-3");
 
-          const cardText = document.createElement("p");
-          cardText.classList.add("card-text");
-          cardText.innerText = comment;
+      const cardBody = document.createElement("div");
+      cardBody.classList.add("card-body");
 
-          cardBody.appendChild(cardTitle);
-          cardBody.appendChild(cardText);
-          card.appendChild(cardBody);
-          container.appendChild(card);
-        });
-      } else {
-        // If comments is not an array, create a card for the single comment
-        const card = document.createElement("div");
-        card.classList.add("card", "mb-3");
+      const cardTitle = document.createElement("h5");
+      cardTitle.classList.add("card-title");
+      cardTitle.innerText = "Comment";
 
-        const cardBody = document.createElement("div");
-        cardBody.classList.add("card-body");
+      const cardText = document.createElement("p");
+      cardText.classList.add("card-text");
+      cardText.innerText = commentObj.comments;
 
-        const cardTitle = document.createElement("h5");
-        cardTitle.classList.add("card-title");
-        cardTitle.innerText = "Comment";
+      const cardLocation = document.createElement("p");
+      cardLocation.classList.add("card-location");
+      // Reverse geocode the latitude and longitude to get the location
+      fetch(`https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${commentObj.latitude}&longitude=${commentObj.longitude}&localityLanguage=en`)
+        .then((response) => response.json())
+        .then((data) => {
+          cardLocation.innerText = data.city + ", " + data.principalSubdivision;
+        })
+        .catch((error) => console.log(error));
 
-        const cardText = document.createElement("p");
-        cardText.classList.add("card-text");
-        cardText.innerText = comments;
+      const cardDate = document.createElement("p");
+      cardDate.classList.add("card-date");
+      // Format the Unix timestamp to a human-readable date and time
+      const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric' };
+      cardDate.innerText = commentObj.created_at.toLocaleString('en-US', options);
 
-        cardBody.appendChild(cardTitle);
-        cardBody.appendChild(cardText);
-        card.appendChild(cardBody);
-        container.appendChild(card);
-      }
+      const cardCondition = document.createElement("p");
+      cardCondition.classList.add("card-condition");
+      cardCondition.innerText = "Condition: " + commentObj.condition;
+
+      cardBody.appendChild(cardTitle);
+      cardBody.appendChild(cardText);
+      cardBody.appendChild(cardLocation);
+      cardBody.appendChild(cardDate);
+      cardBody.appendChild(cardCondition);
+      card.appendChild(cardBody);
+      container.appendChild(card);
     });
   });
 }
