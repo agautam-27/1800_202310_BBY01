840315ed7ecf127acc4311f6619e02f3f1cd664c
Anshul Gautam <agautam9@my.bcit.ca>
2023-03-31 19:28:50 -0700

    fixed some bugs

--- scripts/main.js
+++ scripts/main.js
@@ -1,33 +1,31 @@
 function getNameAndImageFromAuth() {
-    firebase.auth().onAuthStateChanged(user => {
-      // Check if a user is signed in:
-      if (user) {
-        // Do something for the currently logged-in user here:
-        const userName = user.displayName;
-        const userId = user.uid;
-  
-        // Get user document from Firestore
-        firebase.firestore().collection("users").doc(userId).get()
-          .then(doc => {
-            if (doc.exists) {
-              const userData = doc.data();
-              const userImage = userData.profilePic;
-              // Update name and image elements
-              document.getElementById("name-goes-here").innerText = userName;
-              document.getElementById("user-image").src = userImage;
-            } else {
-              console.log("No such document!");
-            }
-          })
-          .catch(error => {
-            console.log("Error getting document:", error);
-          });
-      } else {
-        // No user is signed in.
-      }
-    });
-  }
-  
-  getNameAndImageFromAuth(); //run the function
-  
-  
+  firebase.auth().onAuthStateChanged(user => {
+    // Check if a user is signed in:
+    if (user) {
+      // Do something for the currently logged-in user here:
+      const userId = user.uid;
+
+      // Get user document from Firestore
+      firebase.firestore().collection("users").doc(userId).get()
+        .then(doc => {
+          if (doc.exists) {
+            const userData = doc.data();
+            const userName = userData.name;
+            const userImage = userData.profilePic;
+            // Update name and image elements
+            document.getElementById("name-goes-here").innerText = userName;
+            document.getElementById("user-image").src = userImage;
+          } else {
+            console.log("No such document!");
+          }
+        })
+        .catch(error => {
+          console.log("Error getting document:", error);
+        });
+    } else {
+      // No user is signed in.
+    }
+  });
+}
+
+getNameAndImageFromAuth(); //run the function
